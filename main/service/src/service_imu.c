a911e2abfd224647f841640e4d708c8356b9d1d0
LC <blees1234@qq.com>
2025-10-14 19:08:49 +0800

    ``` fix(service): 注释掉IMU传感器数据打印日志
    将加速度计和陀螺仪数据的打印语句注释掉，避免串口输出过多调试信息影响性能。
    ```

--- main/service/src/service_imu.c
+++ main/service/src/service_imu.c
@@ -76,9 +76,9 @@ static void service_imu_task(void *pvParameters)
         gyro.gyro_z = raw_gyro.raw_gyro_z / gyro_sensitivity;
 
         //打印acce_x, acce_y, acce_z
-        ESP_LOGI(TAG, "acce_x: %f, acce_y: %f, acce_z: %f", acce.acce_x, acce.acce_y, acce.acce_z);
-        //打印gyro_x, gyro_y, gyro_z
-        ESP_LOGI(TAG, "gyro_x: %f, gyro_y: %f, gyro_z: %f", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z*TASK_DELAY_MS/1000.0f);
+        // ESP_LOGI(TAG, "acce_x: %f, acce_y: %f, acce_z: %f", acce.acce_x, acce.acce_y, acce.acce_z);
+        // //打印gyro_x, gyro_y, gyro_z
+        // ESP_LOGI(TAG, "gyro_x: %f, gyro_y: %f, gyro_z: %f", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z*TASK_DELAY_MS/1000.0f);
         
         // 使用互补滤波器计算姿态角
         complimentary_filter(&acce, &gyro, (float)TASK_DELAY_MS/1000.0f);
