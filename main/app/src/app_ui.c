b4a2c5f3f56ccd6ccce2f468d9754272ef669123
sbkj <blees1234@qq.com>
2025-11-04 18:57:56 +0800

    feat(app): 优化状态机与UI表情切换逻辑 - 调整了定时器相关注释格式，提升代码可读性 - 修改表情切换回调函数结构，增强逻辑清晰度 - 优化Gif显示控制逻辑，确保直行与停止状态切换时能及时更新 - 修复左右转Gif资源引用错误问题，统一使用正确资源 - 初始化Gif对象时即加载默认图像，避免空指针风险 - 状态切换后重置对应Gif编号，保证下次切换正常显示

--- main/app/src/app_ui.c
+++ main/app/src/app_ui.c
@@ -439,7 +439,11 @@ int random_express_right(void)
 ***************************************
 ***************************************/
 
-static lv_obj_t *Gif = NULL; // 只创建一次
+static lv_obj_t *Gif = NULL;                                         // 只创建一次
+static Gif_Straight_Num_t Current_Gif_Straight_Num = Gif_Straight_1; // 当前要显示的直行Gif图编号
+static Gif_Straight_Num_t Last_Gif_Straight_Num = Gif_Straight_NULL; // 上一次显示的直行Gif图编号
+static Gif_Stop_Num_t Current_Gif_Stop_Num = Gif_Stop_1;             // 当前要显示的停止Gif图编号
+static Gif_Stop_Num_t Last_Gif_Stop_Num = Gif_Stop_NULL;             // 上一次显示的停止Gif图编号
 void Gif_Ui_Init(void)
 {
     lv_obj_t *scr = lv_scr_act();
@@ -450,11 +454,9 @@ void Gif_Ui_Init(void)
     {
         Gif = lv_gif_create(scr);
         lv_obj_center(Gif);
+        lv_gif_set_src(Gif, &straight1);
     }
 }
-
-static Gif_Straight_Num_t Current_Gif_Straight_Num = Gif_Straight_1; // 当前要显示的直行Gif图编号
-static Gif_Straight_Num_t Last_Gif_Straight_Num = Gif_Straight_NULL; // 上一次显示的直行Gif图编号
 void Gif_Shou_straight(void)
 {
     if (Current_Gif_Straight_Num == Last_Gif_Straight_Num)
@@ -476,6 +478,7 @@ void Gif_Shou_straight(void)
     }
     lvgl_port_unlock();
     Last_Gif_Straight_Num = Current_Gif_Straight_Num; // 更新上一次显示的直行Gif图编号
+    Last_Gif_Stop_Num = Gif_Stop_NULL;                // 播放完后设置停止表情的上一次为NULL，实现直行切换到停止表情时，停止表情能及时更新
 }
 
 // 编写一个直行随机函数，要求随机返回Gif_Straight_Num_t里的枚举，第一次的时候将全部随机排好序，每调用Gif_Straight_Num_MAX次后又重新排序,并更新当前要显示的直行Gif图编号
@@ -507,8 +510,6 @@ Gif_Straight_Num_t random_express_straight(void)
     return express[index++];
 }
 
-static Gif_Stop_Num_t Current_Gif_Stop_Num = Gif_Stop_1; // 当前要显示的停止Gif图编号
-static Gif_Stop_Num_t Last_Gif_Stop_Num = Gif_Stop_NULL; // 上一次显示的停止Gif图编号
 void Gif_Shou_Stop(void)
 {
     if (Current_Gif_Stop_Num == Last_Gif_Stop_Num)
@@ -529,7 +530,8 @@ void Gif_Shou_Stop(void)
         lv_gif_set_src(Gif, &gif1);
     }
     lvgl_port_unlock();
-    Last_Gif_Stop_Num = Current_Gif_Stop_Num; // 更新上一次显示的停止Gif图编号
+    Last_Gif_Stop_Num = Current_Gif_Stop_Num;  // 更新上一次显示的停止Gif图编号
+    Last_Gif_Straight_Num = Gif_Straight_NULL; // 播放完后设置直行表情的上一次为NULL，实现停止切换到直行表情时，直行表情能及时更新
 }
 
 // 编写一个停止随机函数，要求随机返回Gif_Stop_Num_t里的枚举，第一次的时候将全部随机排好序，每调用Gif_Stop_Num_MAX次后又重新排序,并更新当前要显示的停止Gif图编号
@@ -565,7 +567,7 @@ void Gif_Shou_Right(void)
 {
     lvgl_port_lock(0);
     // 显示右转专属GIF图
-    lv_gif_set_src(Gif, &gif2);
+    lv_gif_set_src(Gif, &straight4);
     lvgl_port_unlock();
 
     // 延时3S
@@ -578,8 +580,8 @@ void Gif_Shou_Right(void)
 
     // 保持30S，防止其他状态打断
     vTaskDelay(8 * 1000 / portTICK_PERIOD_MS);
-    Current_Gif_Straight_Num = Gif_Straight_1; // 播放完后设置好切回直行的默认Gif图
-    Current_Gif_Stop_Num = Gif_Stop_1;         // 播放完后设置好切回停止的默认Gif图
+    Last_Gif_Straight_Num = Gif_Straight_NULL; // 播放完后设置好切回直行的默认Gif图
+    Last_Gif_Stop_Num = Gif_Stop_NULL;         // 播放完后设置好切回停止的默认Gif图
 }
 
 // 设置当前要显示的右转完成Gif图，随机返回Gif_Right_Num_t里的枚举，第一次的时候将全部随机排好序，每调用Gif_Right_Num_MAX次后又重新排序,并更新显示当前要显示的右转完成Gif图
@@ -630,7 +632,7 @@ Gif_Right_Num_t random_right_Pass(void)
 void Gif_Shou_Left(void)
 {
     lvgl_port_lock(0);
-    lv_gif_set_src(Gif, &gif2); // 显示左转专属GIF图
+    lv_gif_set_src(Gif, &straight4); // 显示左转专属GIF图
     lvgl_port_unlock();
     vTaskDelay(3 * 1000 / portTICK_PERIOD_MS); // 延时3S
     lvgl_port_lock(0);
@@ -638,8 +640,8 @@ void Gif_Shou_Left(void)
     lvgl_port_unlock();
     // 保持30S，防止其他状态打断
     vTaskDelay(8 * 1000 / portTICK_PERIOD_MS);
-    Current_Gif_Straight_Num = Gif_Straight_1; // 播放完后设置好切回直行的默认Gif图
-    Current_Gif_Stop_Num = Gif_Stop_1;         // 播放完后设置好切回停止的默认Gif图
+    Last_Gif_Straight_Num = Gif_Straight_NULL; // 播放完后设置好切回直行的默认Gif图
+    Last_Gif_Stop_Num = Gif_Stop_NULL;         // 播放完后设置好切回停止的默认Gif图
 }
 
 /*设置当前要显示的左转完成Gif图，随机返回Gif_Left_Num_t里的枚举，
@@ -681,8 +683,7 @@ Gif_Left_Num_t random_left_Pass(void)
     }
     else
     {
-        // 超出范围，设置为默认的gif1
-        lv_gif_set_src(Gif, &gif1);
+        lv_gif_set_src(Gif, &gif1); // 超出范围，设置为默认的gif1
     }
     lvgl_port_unlock();
     return express[index++];
